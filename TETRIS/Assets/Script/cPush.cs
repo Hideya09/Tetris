ddb0c7edabf9a1bfdeaa43d2cbd8015007c5c472
Hideya Sato <satouh@alfasystem.co.jp>
2016-05-06 17:36:07 +0900

    デバッグ後の修正を行いました

---
+++ TETRIS/Assets/Script/cPush.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cPush : MonoBehaviour {
+	private TextMesh m_Text;
+
+	private int m_Sign;
+
+	// Use this for initialization
+	void Start () {
+		m_Text = GetComponent< TextMesh > ();
+
+		m_Sign = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color color = m_Text.color;
+		color.a += Time.deltaTime * m_Sign;
+		m_Text.color = color;
+
+		if (color.a < 0.0f) {
+			m_Sign = 1;
+		}
+
+		if (color.a > 1.0f) {
+			m_Sign = -1;
+		}
+	}
+}
